d436db9722c29b52ed7642971f5c9e243dd43b9c
Rafaela <rafaelagg22@gmail.com>
2025-07-22 21:04:48 -0300

    more functions

--- libft.h
+++ libft.h
@@ -8,6 +8,13 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+
+size_t	ft_strlen(const char *s);
+
 
 /* Parte 2 - Funções adicionais */
 // char	*ft_substr(...);  ← você adicionará aqui depois
--- makefile
+++ makefile
@@ -4,7 +4,11 @@ SRCS= \
 		ft_isalpha.c \
 		ft_isdigit.c \
 		ft_isalnum.c \
-		
+		ft_isascii.c \
+		ft_isprint.c \
+		ft_toupper.c \
+		ft_tolower.c \
+		ft_strlen.c \
 
 
 OBJS= $(SRCS:.c=.o)
