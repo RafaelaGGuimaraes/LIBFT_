b55b10a1afc1961769cf0bdeb40744db78ddd9b5
Rafaela <rafaelagg22@gmail.com>
2025-07-24 19:56:20 -0300

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,11 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgomes-g <rgomes-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rgomes-g <rgomes-g@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 19:23:44 by rgomes-g          #+#    #+#             */
-/*   Updated: 2025/07/23 20:02:38 by rgomes-g         ###   ########.fr       */
+/*   Updated: 2025/07/24 19:44:05 by rgomes-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	while (src[i] && (dst_len + i) < dstsize - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
