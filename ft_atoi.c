3542935c2ea71fb7e7298dbaa7b2d2c7ed8d6740
Rafaela Gomes Guimaraes <rgomes-g@c3r9p4.42sp.org.br>
2025-07-31 21:08:36 -0300

    final

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rgomes-g <rgomes-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:53:27 by rgomes-g          #+#    #+#             */
-/*   Updated: 2025/07/31 18:29:53 by rgomes-g         ###   ########.fr       */
+/*   Updated: 2025/07/31 20:58:06 by rgomes-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@
 int	ft_atoi(char const *str)
 {
 	int	i;
-	int	sinal;
-	int	valor;
+	int	sign;
+	int	result;
 
 	i = 0;
-	sinal = 1;
-	valor = 0;
+	sign = 1;
+	result = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			sinal = -1;
+			sign = -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		valor = (valor * 10) + (str[i] - '0');
+		result = (result * 10) + (str[i] - '0');
 		i++;
 	}
-	return (valor * sinal);
+	return (result * sign);
 }
